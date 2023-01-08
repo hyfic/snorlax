da4ab7ac8b8161e84925b0dbf6557376504a80c7
Fadhil <fadhilsaheer@gmail.com>
2023-01-08 10:29:03 +0530

    required body fields

--- api/router/helper.go
+++ api/router/helper.go
@@ -1,23 +1,21 @@
 package router
 
 import (
-	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 type RequestBody struct { // struct for request which provide path only
-	Path string `json:"path"`
+	Path string `json:"path" form:"path" binding:"required"`
 }
 
 type PutRequestBody struct { // struct for request which provide both, oldPath & newPath, used in renaming folder/file
-	OldPath string `json:"oldPath"`
-	NewPath string `json:"newPath"`
+	OldPath string `json:"oldPath" form:"oldPath" binding:"required"`
+	NewPath string `json:"newPath" form:"newPath" binding:"required"`
 }
 
 func GetBodyFromRequest[T RequestBody | PutRequestBody](context *gin.Context, requestBody *T) error {
-	decoder := json.NewDecoder(context.Request.Body)
-	err := decoder.Decode(&requestBody)
+	err := context.BindJSON(&requestBody)
 
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
