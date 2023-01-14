b8e2f0f56ff903047a4cb3049fbafad64d626a0e
Fadhil <fadhilsaheer@gmail.com>
2023-01-14 20:36:43 +0530

    server checker wrapper

---
+++ app/src/components/serverCheckerWrapper.tsx
@@ -0,0 +1,36 @@
+import SnorlaxLogo from '@/assets/logo.svg';
+import { useServerStore } from '@/store/server.store';
+import { ReactComponent } from '@/types/react.type';
+import { Button } from '@chakra-ui/react';
+import { ServerForm } from './serverOptions/serverFormWrapper';
+
+export const ServerCheckerWrapper: ReactComponent = ({ children }) => {
+  const { selectedServer } = useServerStore();
+
+  if (selectedServer) {
+    return <>{children}</>;
+  } else {
+    return (
+      <div className='w-full h-full flex flex-col justify-center items-center'>
+        <img src={SnorlaxLogo} alt='' className='w-24' />
+        <h2 className='text-xl font-medium text-app-text'>
+          No server connected!
+        </h2>
+        <div className='mt-2 w-full px-60 flex justify-center'>
+          <p className='text-app-text opacity-60 text-center'>
+            You can't use this application unless you connect to a snorlax
+            server.
+          </p>
+        </div>
+        <ServerForm>
+          <Button
+            mt={5}
+            className='text-app-text bg-app-accent transition-all duration-200 hover:bg-app-accent/80'
+          >
+            Add server
+          </Button>
+        </ServerForm>
+      </div>
+    );
+  }
+};
