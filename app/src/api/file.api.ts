0ce26f0d5b57a8721029c770aadd62bc44f6a11a
Fadhil <fadhilsaheer@gmail.com>
2023-01-16 22:16:34 +0530

    get file info api

--- app/src/api/file.api.ts
+++ app/src/api/file.api.ts
@@ -3,3 +3,11 @@ import axios from 'axios';
 export const getDirectory = (connection: string, path: string) => {
   return axios.get(`${connection}/file/view-folder?path=${path}`);
 };
+
+export const getFileInfo = (
+  connection: string,
+  path: string,
+  fileName: string
+) => {
+  return axios.get(`${connection}/file/get-file-info?path=${path}/${fileName}`);
+};
