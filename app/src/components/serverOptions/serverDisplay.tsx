c0a95c8333276f8a30ac9c2a8a16a05e95036515
Fadhil <fadhilsaheer@gmail.com>
2023-01-20 07:18:40 +0530

    added animation

--- app/src/components/serverOptions/serverDisplay.tsx
+++ app/src/components/serverOptions/serverDisplay.tsx
@@ -9,10 +9,19 @@ import { useServerStore } from '@/store/server.store';
 import { showToast } from '@/utils/showToast';
 import { ServerForm } from './serverFormWrapper';
 import { ServerStatus } from './serverStatus';
+import { motion } from 'framer-motion';
 
 interface Props {
   server: ServerType;
 }
+
+const item = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+};
 
 export const ServerDisplay: React.FC<Props> = ({ server }) => {
   const { deleteServer } = useServerStore();
@@ -34,51 +43,56 @@ export const ServerDisplay: React.FC<Props> = ({ server }) => {
   };
 
   return (
-    <Flex
-      className='w-full bg-app-dark3 px-5 py-4 rounded-md border-2 border-app-dark4 border-opacity-50'
-      alignItems='center'
-      justifyContent='space-between'
-      mb={2}
-    >
-      <Flex alignItems='center'>
-        <img src={SnorlaxLogo} className='w-12' alt='' />
-        <div className='ml-3'>
-          <h3 className='font-medium text-app-text text-lg'>{server.name}</h3>
-          <p className='text-app-text2'>{server.connection}</p>
-        </div>
-      </Flex>
-      <Flex alignItems='center'>
-        <ServerStatus className='bg-app-dark4' connection={server.connection} />
-        <PermissionWrapper
-          description='Are you sure you want to remove this server from your list ? You can add server later.'
-          placeholder='Remove server'
-          onClick={serverRemoveHandler}
-        >
-          <Tooltip
-            label='Delete server'
-            className='bg-app-dark3 border border-app-dark4 text-app-text'
+    <motion.div variants={item}>
+      <Flex
+        className='w-full bg-app-dark3 px-5 py-4 rounded-md border-2 border-app-dark4 border-opacity-50'
+        alignItems='center'
+        justifyContent='space-between'
+        mb={2}
+      >
+        <Flex alignItems='center'>
+          <img src={SnorlaxLogo} className='w-12' alt='' />
+          <div className='ml-3'>
+            <h3 className='font-medium text-app-text text-lg'>{server.name}</h3>
+            <p className='text-app-text2'>{server.connection}</p>
+          </div>
+        </Flex>
+        <Flex alignItems='center'>
+          <ServerStatus
+            className='bg-app-dark4'
+            connection={server.connection}
+          />
+          <PermissionWrapper
+            description='Are you sure you want to remove this server from your list ? You can add server later.'
+            placeholder='Remove server'
+            onClick={serverRemoveHandler}
           >
-            <IconButton
-              mx={2}
-              aria-label='Delete server'
-              className='bg-app-dark4 text-app-text hover:opacity-60'
-              icon={<FiTrash2 className='text-lg' />}
-            />
-          </Tooltip>
-        </PermissionWrapper>
-        <ServerForm server={server}>
-          <Tooltip
-            label='Edit server details'
-            className='bg-app-dark3 border border-app-dark4 text-app-text'
-          >
-            <IconButton
-              aria-label='Edit server'
-              className='bg-app-dark4 text-app-text hover:opacity-60'
-              icon={<FiEdit className='text-lg' />}
-            />
-          </Tooltip>
-        </ServerForm>
+            <Tooltip
+              label='Delete server'
+              className='bg-app-dark3 border border-app-dark4 text-app-text'
+            >
+              <IconButton
+                mx={2}
+                aria-label='Delete server'
+                className='bg-app-dark4 text-app-text hover:opacity-60'
+                icon={<FiTrash2 className='text-lg' />}
+              />
+            </Tooltip>
+          </PermissionWrapper>
+          <ServerForm server={server}>
+            <Tooltip
+              label='Edit server details'
+              className='bg-app-dark3 border border-app-dark4 text-app-text'
+            >
+              <IconButton
+                aria-label='Edit server'
+                className='bg-app-dark4 text-app-text hover:opacity-60'
+                icon={<FiEdit className='text-lg' />}
+              />
+            </Tooltip>
+          </ServerForm>
+        </Flex>
       </Flex>
-    </Flex>
+    </motion.div>
   );
 };
