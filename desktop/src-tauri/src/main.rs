d2955045db6b34b9347b2a2e1a539ddfd9a1a66b
Fadhil <fadhilsaheer@gmail.com>
2023-02-27 18:53:35 +0530

    fix: http request not sending

--- desktop/src-tauri/src/main.rs
+++ desktop/src-tauri/src/main.rs
@@ -6,14 +6,26 @@
 mod controller;
 mod database;
 
+use tauri::{utils::config::AppUrl, window::WindowBuilder, WindowUrl};
+
 fn main() {
+    let port = portpicker::pick_unused_port().expect("Failed to find unused port");
+
+    let mut context = tauri::generate_context!();
+    let url = format!("http://localhost:{}", port);
+    let window_url = WindowUrl::External(url.parse().unwrap());
+
+    context.config_mut().build.dist_dir = AppUrl::Url(window_url.clone());
+    context.config_mut().build.dev_path = AppUrl::Url(window_url.clone());
+
     tauri::Builder::default()
+        .plugin(tauri_plugin_localhost::Builder::new(port).build())
         .invoke_handler(tauri::generate_handler![
             controller::server_controller::read_servers,
             controller::server_controller::add_server,
             controller::server_controller::update_server,
             controller::server_controller::delete_server,
         ])
-        .run(tauri::generate_context!())
+        .run(context)
         .expect("error while running tauri application");
 }
